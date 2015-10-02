aa460fe09b04a4e4dfaf44aef06c7358fb562eb6
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-10-03 01:17:34 +0200

    Basic functionality

--- main.go
+++ main.go
@@ -1,6 +1,59 @@
 package main
 
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var (
+	client      *http.Client
+	globalFlags struct {
+		port        int
+		remoteDrive string
+	}
+)
+
+func init() {
+	pool := x509.NewCertPool()
+	pool.AppendCertsFromPEM(pemCerts)
+	client = &http.Client{Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{RootCAs: pool},
+	}}
+
+	flag.IntVar(&globalFlags.port, "port", 80, "The port to listen on")
+	flag.StringVar(&globalFlags.remoteDrive, "remoteDrive",
+		"http://drive.internal.blau.io",
+		"The URL of the Google Drive Service")
+	flag.Parse()
+}
+
 func main() {
-	s := &Site{}
+	log.Printf("Listening on Port %d", globalFlags.port)
+	log.Println("Remote Drive URL: ", globalFlags.remoteDrive)
+
+	http.HandleFunc("/favicon.ico", notfound) //dirty, dirty
+	http.HandleFunc("/", start)
+	http.ListenAndServe(":"+strconv.Itoa(globalFlags.port), nil)
+}
+
+func notfound(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Not found", http.StatusNotFound)
+}
+
+func start(w http.ResponseWriter, r *http.Request) {
+	token, err := r.Cookie("token")
+	if err != nil {
+		log.Printf("Could not get token: %v", err)
+		http.Error(w, "Could not authorize", http.StatusUnauthorized)
+		return
+	}
+
+	s := &Site{
+		Token: token.Value,
+	}
 	s.Build()
 }
--- site.go
+++ site.go
@@ -1,7 +1,114 @@
 package main
 
-type Site struct{}
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/a8m/mark"
+)
+
+type Site struct {
+	BaseURL string
+	Token   string
+}
 
 func (s *Site) Build() error {
+	list, err := s.GetFileList()
+	if err != nil {
+		return err
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(list))
+
+	for _, name := range list {
+		go s.CreatePage(name, wg)
+	}
+
+	wg.Wait()
 	return nil
+}
+
+func (s *Site) CreatePage(name string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	remote := strings.TrimSuffix(globalFlags.remoteDrive, "/") +
+		"/read/blau.io/content/" + name
+	req, err := http.NewRequest("GET", remote, nil)
+	if err != nil {
+		log.Printf("Error while creating request: %v", err)
+		return
+	}
+
+	log.Println("Getting file content")
+
+	req.AddCookie(&http.Cookie{
+		Name:  "token",
+		Value: s.Token,
+	})
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error while making request: %v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error while reading response: %v", err)
+		return
+	}
+
+	name = strings.Replace(name, ".md", ".html", -1)
+	remote = strings.TrimSuffix(globalFlags.remoteDrive, "/") +
+		"/add/blau.io/PUBLIC/" + name
+	reqBody := strings.NewReader(mark.Render(string(body)))
+	req, err = http.NewRequest("PUT", remote, reqBody)
+	if err != nil {
+		log.Printf("Error while creating request: %v", err)
+		return
+	}
+
+	log.Println("Uploading file content")
+
+	req.AddCookie(&http.Cookie{
+		Name:  "token",
+		Value: s.Token,
+	})
+
+	_, err = client.Do(req)
+	if err != nil {
+		log.Printf("Error while making request: %v", err)
+		return
+	}
+}
+
+func (s *Site) GetFileList() ([]string, error) {
+	remote := strings.TrimSuffix(globalFlags.remoteDrive, "/") +
+		"/browse/blau.io/content"
+	req, err := http.NewRequest("GET", remote, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.AddCookie(&http.Cookie{
+		Name:  "token",
+		Value: s.Token,
+	})
+
+	log.Println("Getting file list")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var list []string
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&list)
+	return list, err
 }
